2d16b14bb010f35ea8dbf48080712c44f6c44f9d
Elias Wennerlund <elias06wennerlund@gmail.com>
2025-02-18 17:40:29 +0100

    WIP setting details page

--- lib/cms/admin/routes.rb
+++ lib/cms/admin/routes.rb
@@ -3,21 +3,38 @@ require_relative "../lib"
 
 class AdminRoutes < Sinatra::Base
   before do
-    collections = CMS::Config.collections
-    globals = CMS::Config.globals
+    @collections = CMS::Config.collections
+    @globals = CMS::Config.globals
 
     @settings = []
 
-    if !collections.nil?
-      @settings << { name: "Collections", slug: "collections", items: collections }
+    if !@collections.nil?
+      @settings << { name: "Collections", slug: "collections", items: @collections }
     end
 
-    if !collections.nil?
-      @settings << { name: "Globals", slug: "globals", items: globals }
+    if !@collections.nil?
+      @settings << { name: "Globals", slug: "globals", items: @globals }
     end
   end
 
-  get "/admin/" do
+  get "/admin" do
     erb :index
+  end
+
+  get "/admin/collections" do
+    redirect "/admin" if @collections.empty?
+    redirect "/admin/collections/#{@collections[0].slug}"
+  end
+
+  get "/admin/collections/:slug" do |slug|
+    @setting = @collections.find { |c| c.slug == slug }
+    halt 404 if @setting.nil?
+    erb :setting_details
+  end
+
+  get "/admin/globals/:slug" do |slug|
+    @setting = @globals.find { |g| g.slug == slug }
+    halt 404 if @setting.nil?
+    erb :setting_details
   end
 end
