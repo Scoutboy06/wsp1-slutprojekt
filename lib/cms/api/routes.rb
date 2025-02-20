60d409d377915bd6cc8ee17aa44d04a89f7c9b27
Elias Wennerlund <elias06wennerlund@gmail.com>
2025-02-20 23:27:25 +0100

    WIP Creation of new items (only string/boolean values)

--- lib/cms/api/routes.rb
+++ lib/cms/api/routes.rb
@@ -1,8 +1,50 @@
-require "sinatra/base"
+require "sinatra"
+require "json"
 
 class ApiRoutes < Sinatra::Base
-  get '/api/users/:id' do |id|
+  before do
     content_type :json
-    id.to_i
+
+    @collections = CMS::Config.collections
+    @globals = CMS::Config.globals
+    @db = CMS::Config.db
+
+    @settings = []
+
+    if !@collections.nil?
+      @settings << { name: "Collections", slug: "collections", items: @collections }
+    end
+
+    if !@collections.nil?
+      @settings << { name: "Globals", slug: "globals", items: @globals }
+    end
+  end
+
+  post "/api/collections/:slug" do |slug|
+    @setting = @collections.find { |c| c.slug == slug }
+    halt 404 if @setting.nil?
+
+    data = JSON.parse(request.body.read)
+
+    field_names = @setting.fields.map { |field| field.name }
+    field_values = field_names.map do |name|
+      value = data.fetch(name, nil)
+      value = 1 if value == true
+      value = 0 if value == false
+      value
+    end
+
+    exec_str = "INSERT INTO #{@setting.slug}
+      (#{field_names.join(", ")})
+      VALUES (#{field_names.map { |_| "?" }.join(",")})
+    "
+
+    puts exec_str
+    p field_values
+
+    @db.execute(exec_str, field_values)
+
+    status 201
+    @db.last_insert_row_id.to_json
   end
 end
--- lib/cms/lib/field.rb
+++ lib/cms/lib/field.rb
@@ -1,13 +1,12 @@
 class Field
   attr_reader :name, :type, :required, :default, :relation_to, :fields
 
-  def initialize(name:, type:, required: false, default: nil, relation_to: nil, fields: [])
+  def initialize(name:, type:, required: false, default: nil, relation_to: nil)
     @name = name
     @type = type
     @required = required
     @default = default
     @relation_to = relation_to
-    @fields = fields
   end
 
   def self.from_hash(hash)
@@ -17,7 +16,6 @@ class Field
       required: hash[:required] || false,
       default: hash[:default],
       relation_to: hash[:relation_to],
-      fields: hash[:fields]&.map { |f| Field.from_hash(f) } || [],
     )
   end
 
