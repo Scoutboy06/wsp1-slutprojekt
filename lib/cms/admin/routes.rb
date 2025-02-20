ce339af2fe4ddf66f3d0cf782f8c9ddd11d3dbb6
Elias Wennerlund <elias06wennerlund@gmail.com>
2025-02-20 20:11:39 +0100

    Page for creating new items

--- lib/cms/admin/routes.rb
+++ lib/cms/admin/routes.rb
@@ -32,6 +32,12 @@ class AdminRoutes < Sinatra::Base
     erb :setting_details
   end
 
+  get "/admin/collections/:slug/new" do |slug|
+    @setting = @collections.find { |c| c.slug == slug}
+    halt 404 if @setting.nil?
+    erb :new_setting
+  end
+
   get "/admin/globals/:slug" do |slug|
     @setting = @globals.find { |g| g.slug == slug }
     halt 404 if @setting.nil?
